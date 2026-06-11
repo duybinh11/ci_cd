6e80d4c89df40e2790466988c3ae6001c38a9855
duybinh10 <duybinh1002004@gmail.com>
2026-06-11 11:20:12 +0700

    confg

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+import com.google.firebase.appdistribution.gradle.firebaseAppDistribution
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.appdistribution)
+}
+
+android {
+    namespace = "com.starnest.ci_cd"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.starnest.ci_cd"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            firebaseAppDistribution {
+                artifactType = "APK"
+                releaseNotes = "CI build ${System.getenv("GITHUB_SHA")?.take(7) ?: "local"}"
+                testers = System.getenv("FIREBASE_TESTERS_EMAIL").orEmpty()
+            }
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            firebaseAppDistribution {
+                artifactType = "APK"
+                releaseNotes = "CI build ${System.getenv("GITHUB_SHA")?.take(7) ?: "local"}"
+                testers = System.getenv("FIREBASE_TESTERS_EMAIL").orEmpty()
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
